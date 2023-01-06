d4e43194ba3bcee08f74e6b9f46a34c1a1a3fcd8
jigsaw <m@jgs.me>
2023-01-06 20:30:41 +0900

    add p param

--- netlify/edge-functions/latest_articles.ts
+++ netlify/edge-functions/latest_articles.ts
@@ -1,7 +1,9 @@
 import { latsetArticles } from "../../lib/latestArticles.ts";
 
 export default async function handler(req: Request) {
-  const articles = await latsetArticles();
+  const url = new URL(req.url);
+  const p = parseInt(url.searchParams.get("p") ?? "0");
+  const articles = await latsetArticles(p);
   return new Response(JSON.stringify(articles), {
     status: 200,
     headers: {
