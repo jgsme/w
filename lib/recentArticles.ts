ae845a86c067977c010040566b064d1197730131
jigsaw <m@jgs.me>
2023-01-26 17:13:08 +0900

    clean

--- lib/recentArticles.ts
+++
@@ -1,5 +0,0 @@
-import { client } from "./getSupabaseClient.ts";
-
-export const recentArticles = async () => {
-  return await client.rpc("recent_articles");
-};
--- lib/recentClips.ts
+++
@@ -1,5 +0,0 @@
-import { client } from "./getSupabaseClient.ts";
-
-export const recentClips = async () => {
-  return await client.rpc("recent_clips");
-};
