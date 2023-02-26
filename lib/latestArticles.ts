6ed8dd8c014567ec7e436268f20bb13521710681
jigsaw <m@jgs.me>
2023-02-26 11:58:42 +0900

    add clips page

--- lib/latestArticles.ts
+++
@@ -1,8 +0,0 @@
-import { client } from "./getSupabaseClient.ts";
-
-export const latsetArticles = async (p = 0) => {
-  return await client.rpc("latest_articles", {
-    offset_arg: p * 20,
-    limit_arg: (p + 1) * 20,
-  });
-};
--- lib/latestClips.ts
+++
@@ -1,8 +0,0 @@
-import { client } from "./getSupabaseClient.ts";
-
-export const latsetClips = async (p = 0) => {
-  return await client.rpc("latest_clips", {
-    offset_arg: p * 20,
-    limit_arg: (p + 1) * 20,
-  });
-};
